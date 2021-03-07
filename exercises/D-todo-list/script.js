ca36036bd399a0196671d42a48646e255f17da7a
ademalagoz <ademalagoz46@gmail.com>
2021-03-07 02:36:50 +0000

    Exercises D- E

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,6 +1,31 @@
 function todoList(todos) {
-  // Write your code here...
-}
+
+  let content = document.querySelector("#content");//getting content id
+  let ulTag = document.createElement("ul");//creating ultag
+  
+
+  for(let task of todos){
+    let liTag = document.createElement("li");// creating litag
+    liTag.innerText = task.todo;// assign todo to the litag
+    ulTag.appendChild(liTag);
+    
+liTag.addEventListener("click", linethrough);//adding eventlistener+function
+    function linethrough(){
+      if(liTag.style.textDecorationLine === "line-through"){
+        
+        liTag.style.textDecorationLine = "initial";
+      } 
+      
+      else {
+      liTag.style.textDecorationLine = "line-through";//First click do line-through
+    }
+
+      liTag.remove = "none";//If you click second time, remove it
+  }
+
+  }// for ..of closing
+  content.appendChild(ulTag);
+}// function closing
 
 const todos = [
   {todo: "wash the dishes"}, 
--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,6 +1,23 @@
 function listOfColours(colours) {
-  // Write your code here...
-}
+  let content = document.querySelector("#content");
+  let selectElement = document.createElement("select");//create select element
+  let pElement = document.createElement("p");//create p element
+  content.append(selectElement,pElement);//append them to content
+ 
+for(let colour of colours){ //iterate colours
+  
+  let option = document.createElement("option");
+  option.innerText = colour; //array colour
+  selectElement.appendChild(option);
+ 
+}// for ..of closing
+
+ selectElement.addEventListener("click",()=> {//click+function(choose the colour with value parameter)
+  pElement.innerText = `You have selected :${selectElement.value}`;
+  pElement.style.color = selectElement.value;
+  });
+
+}//function closing
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
 
